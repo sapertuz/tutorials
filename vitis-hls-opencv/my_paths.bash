13b700944c5bac90193e53cea465da17b3d432c9
sapertuz <sergio.pertuz@tu-dresden.de>
2024-09-12 09:07:51 +0200

    add vitis opencv tutorial

---
+++ vitis-hls-opencv/my_paths.bash
@@ -0,0 +1,8 @@
+export MY_VITIS_INSTALL_DIR=/media/psf/share_vm/Xilinx/Vitis
+export MY_VITIS_VERSION="2024.1"
+export MY_OPENCV_DIR=/media/psf/share_vm/bin/opencv
+export MY_OPENCV_INSTALL_DIR=/media/psf/share_vm/install
+
+export OPENCV_INCLUDE=$MY_OPENCV_INSTALL_DIR/include/opencv4
+export OPENCV_LIB=$MY_OPENCV_INSTALL_DIR/lib
+export LD_LIBRARY_PATH=$MY_OPENCV_INSTALL_DIR/lib:$LD_LIBRARY_PATH
